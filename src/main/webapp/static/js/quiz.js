11e2a67c0602ab6d0bc097b0318a1f496e4e4d78
Tomporino <tomorpeter97@gmail.com>
2020-10-15 17:44:21 +0200

    js

--- src/main/webapp/static/js/quiz.js
+++ src/main/webapp/static/js/quiz.js
@@ -1,14 +1,10 @@
 import {getRandomQuestionWithAnswers} from "./main.js";
 
 let QUESTIONBOX = document.querySelector("#question");
-let ANSWERBOX = document.querySelectorAll(".answer");
+let ANSWERBOX = document.querySelectorAll(".answers");
 
 
 
-function game() {
-
-
-}
 
 function refresh() {
     getRandomQuestionWithAnswers(function (question){
@@ -17,11 +13,12 @@ function refresh() {
         let i = 0;
         for (let answer of ANSWERBOX){
             answer.innerHTML = answers[i].answer;
+            answer.setAttribute("data-right", answers[i].rightAnswer)
+            answer.addEventListener('click' ,e => console.log(answer));
             i++;
         }
+
     })
-
-
 }
 
 refresh();
