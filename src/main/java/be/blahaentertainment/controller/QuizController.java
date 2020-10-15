ed85b0be3e185782fdf922514f1cfe63f76d0ac0
Dni <divixed12@gmail.com>
2020-10-16 01:16:00 +0200

    added quiz controller, set htmlpages for engine

--- src/main/java/be/blahaentertainment/controller/QuizController.java
+++ src/main/java/be/blahaentertainment/controller/QuizController.java
@@ -11,14 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/")
-public class MainController extends HttpServlet {
+@WebServlet(urlPatterns = "/quiz")
+public class QuizController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("UTF-8");
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        engine.process("home.html", context, resp.getWriter());
+        engine.process("quiz.html", context, resp.getWriter());
     }
+
+
 }
