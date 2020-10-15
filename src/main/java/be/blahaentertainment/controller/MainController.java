46b64c47e1b9068dde06ad516a684d5f8155581a
Tomporino <tomorpeter97@gmail.com>
2020-10-15 14:24:32 +0200

    rtrt

--- src/main/java/be/blahaentertainment/controller/MainController.java
+++ src/main/java/be/blahaentertainment/controller/MainController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = "/")
 public class MainController extends HttpServlet {
-
+//vsvsvs
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("UTF-8");
