5c72e735d7a79e98c165f4fd91a9c7274e56f639
Dni <divixed12@gmail.com>
2020-10-15 15:33:32 +0200

    sout totest

--- src/main/java/be/blahaentertainment/controller/GetAnswersAPI.java
+++ src/main/java/be/blahaentertainment/controller/GetAnswersAPI.java
@@ -10,15 +10,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/answer")
+@WebServlet(urlPatterns = "/answers")
 public class GetAnswersAPI extends HttpServlet {
     AnswersDao answersDao;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getParameter("questionId"));
         int questionId = Integer.parseInt(req.getParameter("questionId"));
         answersDao = new AnswersDaoJDbc();
         String jsonString = answersDao.getAnswersForQuestion(questionId);
+        System.out.println("answers: "+jsonString);
         resp.setContentType("text/html");
         resp.getWriter().println(jsonString);
 
