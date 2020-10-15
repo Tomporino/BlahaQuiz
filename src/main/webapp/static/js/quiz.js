34d875e3eaeff7f1231f795141ad12c819536100
Tomporino <tomorpeter97@gmail.com>
2020-10-15 14:21:11 +0200

    js files and shit

---
+++ src/main/webapp/static/js/quiz.js
@@ -0,0 +1,10 @@
+import {getRandomQuestion} from "./main";
+import {getAnswersForQuestion} from "./main";
+
+let QUESTIONBOX = document.getElementById("question_box");
+let ANSWERBOX = document.getElementById("answer_box");
+
+
+
+
+
